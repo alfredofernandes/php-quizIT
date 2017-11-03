05d68999282b4759eb8e5a55e9a70c2a4d215ce8
Juliana Lacerda <juli.m.lima@gmail.com>
2017-11-03 00:39:18 -0400

    - implemented result quiz

--- www/Application/User/QuizResult.php
+++ www/Application/User/QuizResult.php
@@ -8,11 +8,61 @@
     </ol>
 </nav>
 <?php 
-    if (isset($_GET['id'])) 
-    {
-		$id = $_GET['id'];
-    }
 
-    echo '<pre>';
-    var_dump($_POST);
-    echo '</pre>';
+    $userId = $_SESSION['userId'];
+
+    if (isset($_POST['userAnswer'])) {
+
+        $answers = $_POST['userAnswer'];
+        $score = 0;
+
+        $quiz_id;
+
+
+        foreach($answers as $quest => $quest_value) {
+
+            $sSQL = "SELECT * FROM Questions WHERE id = '$quest'";
+            $resultSet = mysqli_query($conn, $sSQL);
+
+            $correct_answer = mysqli_fetch_assoc($resultSet);
+
+            if($correct_answer)
+            {
+                $value = $correct_answer['correct_answer'];
+                $quiz_id = $correct_answer['quiz_id'];
+
+                if ($value == $quest_value) {
+                    $score += 1;
+                }
+            }
+
+        }
+
+        $score = $score * 10;
+        //echo $score;
+
+        //inset
+        $sSQL = "INSERT INTO Results (user_id, quiz_id, score) VALUES ('$userId', '$quiz_id', '$score')";
+        $isInserted = mysqli_query($conn, $sSQL);
+
+        $message = "";
+        if ($score >= 80) {
+            $message = "You have successfully passed the test. You are now certified!"; 
+        } else {
+            $message = "Unfortunately you did not pass the test. Please try again later!";
+        }
+
+?>
+
+    <main role="main" class="container">
+        <div class="jumbotron">
+            <h1 class="text-center">Your score was <?php echo $score; ?>%</h1></br></br>
+            <p class="text-center"><?php echo $message; ?></p></br></br>
+
+            <p class="text-center"><a class="btn btn-primary" href="?link=UserResults" role="button">See my results</a></p>
+            
+        </div>
+    </main>
+
+
+<?php } ?>
